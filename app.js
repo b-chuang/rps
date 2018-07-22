b8758b73a6ce48e80d7e299e3840d6b7761802b8
Bill Chuang <34436811+b-chuang@users.noreply.github.com>
2018-07-21 22:17:43 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -18,15 +18,86 @@ function getComputerChoice() {
 // math.floor on math.random will make the random value between 0-3 round down to whole number.
 // return choices [randomNumber] will turn the randomNumber number value --> choices "r,p,s" string value.
 
+function convertToWord(letter) {
+  if (letter === "r") return "Rock";
+  if (letter === "p") return "Paper";
+  return  "Scissors";
+}
+
+function win(userChoice, computerChoice) {
+  const smallUserWord = "user".fontsize(3).sub();
+  const smallCompWord = "comp".fontsize(3).sub();
+  const userChoice_div = document.getElementById(userChoice);
+
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord} = You win!`;
+  userChoice_div.classList.add('green-glow');
+  setTimeout(function() {userChoice_div.classList.remove('green-glow')}, 300);
+}
+
+
+
+function lose(userChoice, computerChoice) {
+  const smallUserWord = "user".fontsize(3).sub();
+  const smallCompWord = "comp".fontsize(3).sub();
+  const userChoice_div = document.getElementById(userChoice);
+
+  computerScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(computerChoice)}${smallCompWord} = You lost!`;
+   userChoice_div.classList.add('red-glow');
+  setTimeout(function() {userChoice_div.classList.remove('red-glow')}, 300);
+}
+
+
+
+function draw(userChoice, computerChoice) {
+  const smallUserWord = "user".fontsize(3).sub();
+  const smallCompWord = "comp".fontsize(3).sub();
+  const userChoice_div = document.getElementById(userChoice);
+
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals ${convertToWord(computerChoice)}${smallCompWord} = Draw!`;
+   userChoice_div.classList.add('grey-glow');
+  setTimeout(function() {userChoice_div.classList.remove('grey-glow')}, 300);
+}
+
+
 
 function game(userChoice) {
   const computerChoice = getComputerChoice();
-  console.log("user choice => " + userChoice);
-  console.log("computer choice => " + computerChoice);
-}
 // make a function with name of game (which grabs the userChoice)
-// "game" will definie computerChoice as const and runs the function getComputerChoice
+// "game" will define computerChoice as const and runs the function getComputerChoice
 //
+
+  switch (userChoice + computerChoice) {
+    case "rs":
+    case "pr":
+    case "sp":
+      win(userChoice, computerChoice);
+      break;
+    case "rp":
+    case "ps":
+    case "sr":
+      lose(userChoice, computerChoice);
+      break;
+    case "rr":
+    case "pp":
+    case "ss":
+      draw(userChoice, computerChoice);
+      break;
+  }
+}
+
+  // console.log("user choice => " + userChoice);
+  // console.log("computer choice => " + computerChoice);
+
+
+
 
 
 function main() {
